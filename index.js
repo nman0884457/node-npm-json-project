3f96407801a53048a64e9ace2ff4aff5230ba6f1
Nick <nm0884457@otc.edu>
2017-11-01 08:56:41 -0500

    Added readJson and Lodash

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-var chalk = require('chalk');
+var _ = require('lodash');
+var readJson = require('read-json-file');
+ 
+readJSON(pathToYourJson, function(error, data){
+    if (error) {
+        throw error;
+    }
+    console.log(data);
+});
